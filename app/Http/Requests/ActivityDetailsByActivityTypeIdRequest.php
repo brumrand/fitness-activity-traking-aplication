bc71f06495b986fc2b15fcd6c4a6b1ac13876ce4
brunfrag <brfraga99@gmail.com>
2024-04-10 12:34:47 +0200

    feat(): creating request and validation rules for the endpoints

--- app/Http/Requests/ActivityDetailsByActivityTypeIdRequest.php
+++ app/Http/Requests/ActivityDetailsByActivityTypeIdRequest.php
@@ -11,7 +11,7 @@ class ActivityDetailsByActivityTypeIdRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,14 @@ class ActivityDetailsByActivityTypeIdRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'user_id'                        => 'required|exists:users,id',
+            'activity_type_id'           => 'required|exists:activity_types,id',
+            'distance_unit_id'          => 'required|exists:distance_units,id',
+            'elapsed_time_unit_id'  => 'required|exists:elapsed_time_units,id',
+            'name'                          => 'required|string|max:255',
+            'distance'                      => 'required|numeric|min:0',
+            'elapsed_time'              => 'required|numeric|min:0',
+            'date'                            => 'required|date',
         ];
     }
 }
--- app/Http/Requests/ActivityDetailsPostRequest.php
+++ app/Http/Requests/ActivityDetailsPostRequest.php
@@ -11,7 +11,7 @@ class ActivityDetailsPostRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class ActivityDetailsPostRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+                'activity_type_id' => 'required|numeric|exists:activity_types,id',
         ];
     }
 }
