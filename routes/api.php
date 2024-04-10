b001a52f3e2c76b2f83f0acc6c439b2c817055c2
brunfrag <brfraga99@gmail.com>
2024-04-10 12:19:26 +0200

    feat(api) : make routes for the endpoints

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ActivityDetailsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::get("activites", [ActivityDetailsController::class, "getActivitiesDetails"]);
+Route::get("activites/byType", [ActivityDetailsController::class, "getActivitiesDetailsByActivityType"]);
+Route::get("activites/byType/distance", [ActivityDetailsController::class, "getDistanceByActivityType"]);
+Route::get("activites/byType/elapsedTime", [ActivityDetailsController::class, "getElapsedTimeByActivityType"]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post("activity", [ActivityDetailsController::class, "post"]);
+Route::delete("activity", [ActivityDetailsController::class, "delete"]);
+
