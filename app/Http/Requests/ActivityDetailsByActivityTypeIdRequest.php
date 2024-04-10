60544de3a226009462e979b2ed10b1eb41efcfa9
brunfrag <brfraga99@gmail.com>
2024-04-10 12:50:35 +0200

    fix(): change validation rules between requests

--- app/Http/Requests/ActivityDetailsByActivityTypeIdRequest.php
+++ app/Http/Requests/ActivityDetailsByActivityTypeIdRequest.php
@@ -22,14 +22,8 @@ class ActivityDetailsByActivityTypeIdRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'user_id'                        => 'required|exists:users,id',
-            'activity_type_id'           => 'required|exists:activity_types,id',
-            'distance_unit_id'          => 'required|exists:distance_units,id',
-            'elapsed_time_unit_id'  => 'required|exists:elapsed_time_units,id',
-            'name'                          => 'required|string|max:255',
-            'distance'                      => 'required|numeric|min:0',
-            'elapsed_time'              => 'required|numeric|min:0',
-            'date'                            => 'required|date',
+            'activity_type_id' => 'required|numeric|exists:activity_types,id',
+
         ];
     }
 }
