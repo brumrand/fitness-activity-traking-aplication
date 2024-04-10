c81d17dba55de1fd174af46dbc344b43de468994
brunfrag <brfraga99@gmail.com>
2024-04-10 11:10:35 +0200

    feat(): completeelapsed tyme type  migration and add relations to the model

--- app/Models/ElapsedTimeUnit.php
+++ app/Models/ElapsedTimeUnit.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ElapsedTimeUnit extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name'
+    ];
+    public function activityDetail(): HasMany
+    {
+        return $this->hasMany(ActivityDetail::class);
+    }
 }
--- database/migrations/2024_04_10_084351_create_elapsed_time_units_table.php
+++ database/migrations/2024_04_10_084351_create_elapsed_time_units_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('elapsed_time_units', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
             $table->timestamps();
         });
     }
